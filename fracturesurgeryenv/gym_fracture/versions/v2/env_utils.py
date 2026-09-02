f5184bbb230dae972ca514eb44b45839c50979a3
CatherineMAbraham <catherinemabraham@gmail.com>
2026-09-02 07:20:07 +0100

    changed reward in contact to only care about contact

--- fracturesurgeryenv/gym_fracture/versions/v2/env_utils.py
+++ fracturesurgeryenv/gym_fracture/versions/v2/env_utils.py
@@ -224,7 +224,7 @@ def set_observation(env, pos, ori, vel, jointPoses, jointVelocities,
         env.achieved_goal = np.array(list(pos) + [isHolding]+[force])#+[env.contact])
         env.desired_goal = np.array(list(env.goal_pos) + [1]+desired_force)#+object_contact)
     elif env.contact_type == 1:
-        env.achieved_goal = np.array(list(pos) + list(ori) + [isHolding]+[force]+[anycontact])#+[env.contact])
+        env.achieved_goal = np.array(list(pos) + list(ori) + [isHolding]+[force]+[contact])#+[env.contact])
         env.desired_goal = np.array(list(env.target_position) + [1]+desired_force +object_contact)
     else:
         env.achieved_goal = np.array(list(pos) + list(ori) + [isHolding]+[force])#we're going to set this as 'success' so we don't really look for it but it keeps the shape the same.
